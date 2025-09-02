f169ee3624a3c313075f6269f3d36d7c32f5b237
livejayo <jeff.oliver@live.ca>
2025-09-02 10:14:52 -0700

    Configure routing for public subnets

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-# STAGE 1: VPC ONLY
-
 terraform {
   required_providers {
     aws = {
@@ -13,11 +11,64 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main-vpc"
+    Name = "my-test-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my-test-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = 3
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_subnet" "public_subnets" {
+  count                   = 3
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.my_vpc.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = 3
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.my_vpc.cidr_block, 8, count.index + 3)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
   }
 }
